59c75c357bd51b24820ecb7da752cbac0cdd2054
HowlOfTheVoid <ehgraupmann@gmail.com>
2025-02-04 11:03:11 -0500

    Began implementing redux, development interrupted

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './ShoppingCart.css'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice'; // Assuming you have action creators for increasing and decreasing item quantity
+import './ShoppingCart.css';
 
 const ShoppingCart = () => {
 
